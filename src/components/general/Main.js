6b0f6541044a9779e5de83051fd53219b05eef76
Angel Cruz <angi_ed01@hotmail.com>
2021-08-08 14:15:46 -0500

    General Cascaron

--- src/components/general/Main.js
+++ src/components/general/Main.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { Navbar } from './navbar/Navbar'
 import { Texture } from './texture/Texture'
 
 const itemsMenu = [
-    { name: "Solicitud de documento", icon: "fas fa-file" },
-    { name: "Consulta de calificaciones", icon: "fas fa-folder-open" },
-    { name: "Registro de gastos", icon: "" },
-    { name: "Realizar pago", icon: "fas fa-money-bill" },
-    { name: "Abonos", icon: "fas fa-coins" },
-    { name: "Consultar estado de pago", icon: "fas fa-money-check-alt" },
+    { name: "Solicitud de documento", icon: "fas fa-file", css: { transform: "rotate(90deg) scaleX(-1)" } },
+    { name: "Consulta de calificaciones", icon: "fas fa-folder-open", css: {} },
+    { name: "Registro de gastos", icon: "fas fa-ticket-alt", css: {} },
+    { name: "Realizar pago", icon: "fas fa-money-bill", css: {} },
+    { name: "Abonos", icon: "fas fa-coins", css: {} },
+    { name: "Consultar estado de pago", icon: "fas fa-money-check-alt", css: {} },
     // { name: "Captura de datos" }
 ]
 
@@ -22,15 +22,15 @@ export const Main = () => {
                 <div className="general">
                     <div className="general__menu">
                         {itemsMenu.map((item, index) => (
-                            <div className="general__menu__item">
-                                <i className={`${item.icon}`}></i>
-                                <Link to={`/${item.name}`} key={index}>{item.name}</Link>
-                            </div>
+                            <NavLink className="general__menu__item" activeClassName="active" to={`/${item.name.replace('%20', '_').toLowerCase()}`} key={index}>
+                                <i className={`${item.icon}`} style={item.css}></i>
+                                <p>{item.name}</p>
+                            </NavLink>
                         ))}
 
                     </div>
                     <div className="general__overtexture">
-                        <h1>Hola a todos, Rafa no se baña. </h1>
+                        <h1></h1>
                     </div>
                 </div>
 
