878ecfd3277684fe9e67f83a465b7551d5247b9b
Francisco Rafael <rafudo.beltran@gmail.com>
2021-08-08 22:24:03 -0500

    Commit Control

--- src/types/types.js
+++ src/types/types.js
@@ -1,7 +1,4 @@
 export const types = {
-<<<<<<< HEAD
-
-=======
     
     authCheckingStart: '[auth] Start checking login state',
     authCheckingFinish: '[auth] Finish checking login state',
@@ -10,5 +7,4 @@ export const types = {
     authStartRegister: '[auth] Start Register',
     authStartTokenRenew: '[auth] Start token renew',
     authLogout: '[auth] Logout',
->>>>>>> login
 }
